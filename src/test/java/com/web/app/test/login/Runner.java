419ade8eb6e819823b0a39ef1a2ddab9042cc995
Raghu Seethamraju <rkiran-consultant@scholastic.com>
2022-02-12 03:35:45 +0530

    first commmit

--- src/test/java/com/web/app/test/login/Runner.java
+++
@@ -1,7 +0,0 @@
-package com.web.app.test.login;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-@io.cucumber.testng.CucumberOptions(features = "src/test/java/features/login.feature", glue = {"com/web/app/test/login"})
-public class Runner extends AbstractTestNGCucumberTests{
-}
